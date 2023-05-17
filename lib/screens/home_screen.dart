44f26a37a351090ab8e590b62d1a70f4d9228a37
leejss <goldemshine@gmail.com>
2023-05-17 23:34:47 +0900

    bottom nav

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,67 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Body(
-        content: Content(
-          child: Container(
-            child: Image.network("https://picsum.photos/250?image=9"),
-          ),
+    return Scaffold(
+      body: Center(child: Text("Waiting for images...")),
+      bottomNavigationBar: Container(
+        alignment: Alignment.center,
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(80), color: Colors.black),
+              child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(foregroundColor: Colors.white),
+                  child: Text(
+                    "홈",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+            )
+          ],
         ),
-        bottom: BottomNavigation());
-  }
-}
-
-class Body extends StatelessWidget {
-  const Body({super.key, required this.content, required this.bottom});
-
-  final Widget content;
-  final Widget bottom;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [Expanded(child: content), bottom],
-    );
-  }
-}
-
-class Content extends StatelessWidget {
-  const Content({super.key, required this.child});
-
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(color: Colors.white),
-      child: child,
-    );
-  }
-}
-
-class BottomNavigation extends StatelessWidget {
-  const BottomNavigation({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 64,
-      decoration: const BoxDecoration(
-          color: Color.fromRGBO(255, 255, 255, 1),
-          border: Border(top: BorderSide(color: Colors.black))),
-      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        ElevatedButton(
-          child: const Icon((Icons.home)),
-          onPressed: () {
-            context.go("/");
-          },
-        )
-      ]),
+      ),
     );
   }
 }
