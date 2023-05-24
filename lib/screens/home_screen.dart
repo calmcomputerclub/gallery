2d1473e753586a0f64a40e543a42632c479c8bf0
leejss <goldemshine@gmail.com>
2023-05-24 22:53:54 +0900

    loading shimmer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:gallery/widget/image_grid.dart';
 import 'package:gallery/widget/navigation.dart';
+import 'package:gallery/widget/shimmer_loading.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Padding(
           padding: EdgeInsets.all(20),
           child: Column(
-            children: [Navigation()],
+            children: [
+              Navigation(),
+              SizedBox(height: 30),
+              Expanded(
+                  child: ShimmerLoading(isLoading: true, child: ImageGrid()))
+            ],
           )),
     );
   }
